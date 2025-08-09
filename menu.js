290ebcb018d4b2b0d3f2d9e0b98ea3404d998c94
4e5757r666766 <homakmargarita29@gmail.com>
2025-08-09 13:32:25 +0300

    comment

---
+++ menu.js
@@ -0,0 +1,11 @@
+let button = document.querySelector(".button")
+let price = botton.replaceAll("грн", "")
+price = number(botton)
+let totalPrice_field = document.querySelector(".totalPrice")
+buttons(){
+    price = 0
+    price =+ price
+    totalPrice_field = price
+    console.log(totalPrice_field)
+}
+addEventListener(click , buttons)
